ce5ded46966e73236eede2ec99db0f0896c0e1ed
Andrey Tsapkov <tsapkov1994@gmail.com>
2022-05-10 17:53:47 +0300

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
-const refs = {};
+const refs = {
+  buttonStart: document.querySelector('[data-start]'),
+  buttonStop: document.querySelector('[data-stop]'),
+};
+refs.buttonStop.disabled = true;
+refs.buttonStart.addEventListener('click', onBodyChangeBackgroundColor);
+refs.buttonStop.addEventListener('click', onBodyStopChangeBackgroundColor);
+
+function onBodyChangeBackgroundColor() {
+  timerID = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.buttonStart.disabled = true;
+  refs.buttonStop.disabled = false;
+}
+
+function onBodyStopChangeBackgroundColor() {
+  clearInterval(timerID);
+  refs.buttonStart.disabled = false;
+  refs.buttonStop.disabled = true;
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
